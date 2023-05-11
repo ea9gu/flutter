7e72e3ea181d6e62c3417f4974164a2bc3fc694c
Youmin Kim <march02198@gmail.com>
2023-05-11 16:36:52 +0900

    feat: device id,type

--- lib/device_info.dart
+++ lib/device_info.dart
@@ -3,14 +3,25 @@ import 'package:flutter/services.dart';
 import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 
-Future<String?> getDeviceId() async {
+Future<Map<String, String>> getDeviceInfo() async {
+  Map<String, dynamic> deviceData = <String, dynamic>{};
   final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
-  if (Platform.isAndroid) {
-    final AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
-    return androidInfo.androidId; // 안드로이드 기기 ID
-  } else if (Platform.isIOS) {
-    final IosDeviceInfo iosInfo = await deviceInfoPlugin.iosInfo;
-    return iosInfo.identifierForVendor; // iOS 기기 ID
+  String device_id = '';
+  String device_type = '';
+
+  try {
+    if (Platform.isAndroid) {
+      final AndroidDeviceInfo androidInfo = await deviceInfoPlugin.androidInfo;
+      device_id = androidInfo.androidId; // 안드로이드 기기 ID
+      device_type = androidInfo.model;
+    } else if (Platform.isIOS) {
+      final IosDeviceInfo iosInfo = await deviceInfoPlugin.iosInfo;
+      device_id = iosInfo.identifierForVendor; // iOS 기기 ID
+      device_type = iosInfo.model;
+    }
+  } on PlatformException {
+    device_id = '';
+    device_type = '';
   }
-  return null;
+  return {'device_type': device_type, 'device_id': device_id};
 }
