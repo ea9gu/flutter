31bb11d7cb36fdc737d1171c0d8726da147d94d1
Youmin Kim <march02198@gmail.com>
2023-05-16 23:00:10 +0900

    Feat: add current device

--- lib/Screens/Student/device.dart
+++ lib/Screens/Student/device.dart
@@ -4,6 +4,7 @@ import 'package:ea9gu/device_info.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:ea9gu/api/add_device.dart';
+import 'package:http/http.dart' as http;
 
 class Device extends StatefulWidget {
   @override
@@ -12,10 +13,15 @@ class Device extends StatefulWidget {
 
 class _DeviceScreenState extends State<Device> {
   Map<String, String> deviceInfo = {};
+  final student_id = "2300000";
+  String current_id = "";
+  String current_name = "";
+  String current_time = "";
 
   @override
   void initState() {
     super.initState();
+    getCurrentDevice();
     _getDeviceInfo();
   }
 
@@ -28,7 +34,11 @@ class _DeviceScreenState extends State<Device> {
     final deviceType = deviceInfo['device_type'];
     final deviceSerial = deviceInfo['device_id'];
 
-    final response = await addDevice(deviceType!, deviceSerial!);
+    final response = await addDevice(
+      student_id,
+      deviceType!,
+      deviceSerial!,
+    );
 
     final responseData = jsonDecode(response.body);
     final status = responseData['status'];
@@ -40,7 +50,7 @@ class _DeviceScreenState extends State<Device> {
         DialogFormat.customDialog(
           context: context,
           title: 'Error',
-          content: '이미 등록된 디바이스입니다.',
+          content: '이미 디바이스를 등록한 id입니다. 디바이스 변경은 등록일로부터 2주 후 가능합니다.',
         );
       } else {
         //print('기기 등록에 실패했습니다.');
@@ -57,6 +67,29 @@ class _DeviceScreenState extends State<Device> {
         title: 'Success',
         content: '기기 등록에 성공했습니다.',
       );
+    }
+    getCurrentDevice();
+  }
+
+  Future<void> getCurrentDevice() async {
+    final response = await getCurrentDeviceInfo(
+      student_id,
+    );
+
+    final responseData = jsonDecode(response.body);
+    //print(responseData);
+    final status = responseData['status'];
+
+    if (status == 'success') {
+      print('등록된 디바이스가 있습니다.');
+      setState(() {
+        current_id = responseData['device_id'];
+        current_name = responseData['device_name'];
+        current_time = responseData['timestamp'];
+      });
+      print(current_id);
+    } else {
+      throw Exception('등록된 디바이스가 없습니다.');
     }
   }
 
@@ -87,12 +120,18 @@ class _DeviceScreenState extends State<Device> {
                   SizedBox(height: 5),
                   Text(
                       "출석체크를 위해서는 기기를 등록 하셔야 합니다.\n등록한 기기는 2주 이내에 변경이 불가능합니다."),
-                  Text('현재 접속 기기 : ${deviceInfo['device_type'] ?? ''}'),
+                  Text(
+                    '현재 접속 기기 : ${deviceInfo['device_type'] ?? ''}',
+                    style: TextStyle(
+                      color: Color(0xff8685A6),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                   SizedBox(height: 10),
                   NextButton(
                       text: "기기등록",
                       onpress: () {
-                        print(deviceInfo['device_id']);
+                        //print(deviceInfo['device_id']);
                         registerDevice();
                       }),
                   SizedBox(height: 15),
@@ -100,6 +139,51 @@ class _DeviceScreenState extends State<Device> {
                       height: 1.0, width: 500.0, color: Color(0xffD8DADC)),
                   SizedBox(height: 15),
                   Text("현재 등록 기기"),
+                  SizedBox(height: 10),
+                  current_id != ""
+                      ? Container(
+                          height: 80,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                            border: Border.all(
+                              width: 3,
+                              color: Color(0xff8685A6),
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.smartphone, // 왼쪽에 표시할 아이콘
+                                  color: Colors.black,
+                                  size: 50.0),
+                              SizedBox(width: 10.0), // 아이콘과 글씨 사이의 간격 조정
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "${current_name} - ${current_id}", // 첫 번째 줄로 표시할 글씨
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 17.0, // 첫 번째 줄 글씨 크기 조정
+                                        // 진한 글씨체 적용
+                                      ),
+                                    ),
+                                    Text(
+                                      current_time, // 두 번째 줄로 표시할 글씨
+                                      style: TextStyle(
+                                        color: Colors.grey, // 연한 색상 적용
+                                        fontSize: 14.0, // 두 번째 줄 글씨 크기 조정
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Text("등록된 기기가 없습니다.")
                 ],
               ),
             ),
--- lib/api/add_device.dart
+++ lib/api/add_device.dart
@@ -1,15 +1,31 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<http.Response> addDevice(String deviceType, String deviceSerial) async {
+Future<http.Response> addDevice(
+    String student_id, String deviceType, String deviceSerial) async {
   final url = Uri.parse('http://10.0.2.2:8000/serial/save-device/');
 
   final response = await http.post(
     url,
     headers: {'Content-Type': 'application/json'},
     body: jsonEncode({
+      'student_id': student_id,
       'device_name': deviceType,
       'device_serial': deviceSerial,
+    }),
+  );
+
+  return response;
+}
+
+Future<http.Response> getCurrentDeviceInfo(String student_id) async {
+  final url = Uri.parse('http://10.0.2.2:8000/serial/get-device/');
+
+  final response = await http.post(
+    url,
+    headers: {'Content-Type': 'application/json'},
+    body: jsonEncode({
+      'student_id': student_id,
     }),
   );
 
